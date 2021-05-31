95bb4c1a7344ede807c52773f9f43be3c75e30c0
antoniocuadros <antculap@gmail.com>
2021-05-31 17:53:13 +0200

    Avanza #7, añadido fragmento categorías

---
+++ src/app/src/main/java/com/acl/easymeal/fragmentos/FragmentoCategorias.kt
@@ -0,0 +1,24 @@
+package com.acl.easymeal.fragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.acl.easymeal.R
+
+
+class FragmentoCategorias : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragmento_categorias, container, false)
+    }
+
+    
+}
