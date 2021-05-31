9f1252a175fc4c562284e7734c9d98a97383bac9
antoniocuadros <antculap@gmail.com>
2021-05-31 19:43:12 +0200

    closes #4, añadida clase Valoracion

--- src/app/src/main/java/com/acl/easymeal/modelo/Valoracion.kt
+++ src/app/src/main/java/com/acl/easymeal/modelo/Valoracion.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "Valoraciones")
-data class Valoracion(
+data class Valoracion( 
     @PrimaryKey val idReceta:String,
     val valoracionMedia:String,
     val totalValoraciones:ArrayList<Int>
