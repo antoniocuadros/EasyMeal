d34fe065bacef4500290623e7fc01c5809b75ee1
antoniocuadros <antculap@gmail.com>
2021-05-31 17:45:42 +0200

    Closes #2, añadido menu inferior

--- src/app/src/main/java/com/acl/easymeal/MainActivity.kt
+++ src/app/src/main/java/com/acl/easymeal/MainActivity.kt
@@ -2,10 +2,21 @@ package com.acl.easymeal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inicializaControlNavegacion()
+    }
+
+    private fun inicializaControlNavegacion(){
+        val menu_inferior = findViewById<BottomNavigationView>(R.id.menu_inferior)
+        val controlador_navegacion = findNavController(R.id.fragment2)
+        menu_inferior.setupWithNavController(controlador_navegacion)
     }
 }
