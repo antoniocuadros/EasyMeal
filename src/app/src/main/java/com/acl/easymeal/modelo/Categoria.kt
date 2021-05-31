37b8fff33d49bf5396e0b5a4a09dae82e6277218
antoniocuadros <antculap@gmail.com>
2021-05-31 19:28:00 +0200

    Avanza #4, añadida clase categoria

--- src/app/src/main/java/com/acl/easymeal/modelo/Categoria.kt
+++ src/app/src/main/java/com/acl/easymeal/modelo/Categoria.kt
@@ -1,7 +1,9 @@
 package com.acl.easymeal.modelo
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity(tableName = "Categorias")
 data class Categoria(
     @PrimaryKey val nombreCategoria:String,
     val imagen:String
