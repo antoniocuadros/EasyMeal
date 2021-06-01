8217698813daf5201c04e4e33df8fad6be56e448
antoniocuadros <antculap@gmail.com>
2021-06-01 12:35:25 +0200

    Avanza #5, añadido Dao para Receta

---
+++ src/app/src/main/java/com/acl/easymeal/modelo/RecetaDao.kt
@@ -0,0 +1,28 @@
+package com.acl.easymeal.modelo
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+interface RecetaDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertaUna(item:Receta)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertaLista(lista:MutableList<Receta>)
+
+    @Query("SELECT * FROM Recetas")
+    fun obtenerTodas():MutableList<Receta>
+
+    @Query("SELECT * FROM Recetas WHERE nombreReceta= :buscar")
+    fun obtenerPorNombre(buscar:String):MutableList<Receta>
+
+    @Query("SELECT * FROM Recetas WHERE categoria= :buscar")
+    fun obtenerPorCategoría(buscar:String):MutableList<Receta>
+
+    @Query("SELECT * FROM Recetas WHERE ingrediente_principal= :buscar")
+    fun obtenerPorIngredientePrincipal(buscar:String):MutableList<Receta>
+
+    @Query("SELECT * FROM Recetas WHERE idAutor= :buscar")
+    fun obtenerPorAutor(buscar:String):MutableList<Receta>
+}
