f174154049a4ddede6a205986edfd65555783ce8
antoniocuadros <antculap@gmail.com>
2021-06-01 12:42:53 +0200

    Avanza #5, añadido Dao para Valoracion

---
+++ src/app/src/main/java/com/acl/easymeal/modelo/ValoracionDao.kt
@@ -0,0 +1,17 @@
+package com.acl.easymeal.modelo
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+interface ValoracionDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertaUna(item:Valoracion)
+
+    @Update
+    fun actualizaValoracion(item:Valoracion)
+
+    @Query("SELECT * FROM Valoraciones WHERE idReceta= :buscar")
+    fun obtenerPorNombre(buscar:String):MutableList<Valoracion>
+}
