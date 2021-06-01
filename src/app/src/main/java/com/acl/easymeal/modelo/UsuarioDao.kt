b88d876e8f8dd3de44169357b2066bd80a5721f5
antoniocuadros <antculap@gmail.com>
2021-06-01 12:38:30 +0200

    Avanza #5, añadido Dao para Usuario

---
+++ src/app/src/main/java/com/acl/easymeal/modelo/UsuarioDao.kt
@@ -0,0 +1,20 @@
+package com.acl.easymeal.modelo
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+interface UsuarioDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertaUna(item:Usuario)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertaLista(lista:MutableList<Usuario>)
+
+    @Query("SELECT * FROM Usuarios")
+    fun obtenerTodas():MutableList<Usuario>
+
+    @Query("SELECT * FROM Usuarios WHERE username= :buscar")
+    fun obtenerPorNombre(buscar:String):MutableList<Usuario>
+
+}
