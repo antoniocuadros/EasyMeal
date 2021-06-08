022c22ca6ed7b1ba1b786c3a8fb6364ef29958e2
antoniocuadros <antculap@gmail.com>
2021-06-08 11:27:26 +0200

    Avanza #36 ya que atributo añadido y typeconverter actualizado para permitir imágenes nulas

--- src/app/src/main/java/com/acl/easymeal/ConversorImagen.kt
+++ src/app/src/main/java/com/acl/easymeal/ConversorImagen.kt
@@ -5,22 +5,29 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.room.TypeConverter
 import java.io.ByteArrayOutputStream
+import java.lang.Exception
 
 class ConversorImagen {
 
     @TypeConverter
-    public fun bitmapToByteArray(imagenBitmap:Bitmap):ByteArray{
-        val streamSalida:ByteArrayOutputStream
-        streamSalida = ByteArrayOutputStream()
+    public fun bitmapToByteArray(imagenBitmap:Bitmap?):ByteArray?{
+            val streamSalida:ByteArrayOutputStream
+            streamSalida = ByteArrayOutputStream()
 
-        imagenBitmap.compress(Bitmap.CompressFormat.PNG, 100, streamSalida)
-        return streamSalida.toByteArray()
+            imagenBitmap?.compress(Bitmap.CompressFormat.PNG, 100, streamSalida)
+            return streamSalida.toByteArray()
     }
 
     @TypeConverter
-    public fun byteArrayToBitmap(imagenByteArray: ByteArray):Bitmap{
-        val bitmap:Bitmap = BitmapFactory.decodeByteArray(imagenByteArray, 0, imagenByteArray.size)
-        return bitmap
+    public fun byteArrayToBitmap(imagenByteArray: ByteArray?):Bitmap?{
+        try {
+            val bitmap:Bitmap = BitmapFactory.decodeByteArray(imagenByteArray!!, 0, imagenByteArray?.size!!)
+            return bitmap
+        }catch (e:Exception){
+            return null
+        }
+
+
     }
 
     public fun stringToID(nombre:String, context:Context):Int{
