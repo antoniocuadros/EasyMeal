6b7c3758c7104037027d86588304768138a2bbd4
antoniocuadros <antculap@gmail.com>
2021-06-08 21:45:07 +0200

    Closes #37 todo comentado

--- src/app/src/main/java/com/acl/easymeal/ConversorImagen.kt
+++ src/app/src/main/java/com/acl/easymeal/ConversorImagen.kt
@@ -7,8 +7,17 @@ import androidx.room.TypeConverter
 import java.io.ByteArrayOutputStream
 import java.lang.Exception
 
+/*
+    Esta clase aporta los métodos necesarios para convertir imágenes de bytes a bitmap y viceversa.
+    Esto será utilizado para almacenar y recuperar los datos de la base de datos. Room hace uso de
+    los typeConverters para hacer esto de forma automática.
+ */
 class ConversorImagen {
 
+    /*
+        Este método transforma una imagen proporcionada como un array de bytes en un objeto de tipo
+        bitmap que es como las manejamos en la aplicación.
+     */
     @TypeConverter
     public fun bitmapToByteArray(imagenBitmap:Bitmap?):ByteArray?{
             val streamSalida:ByteArrayOutputStream
@@ -18,6 +27,10 @@ class ConversorImagen {
             return streamSalida.toByteArray()
     }
 
+    /*
+        Este método transforma una imagen proporcionada como un objeto de tipo bitmap a un array de bytes
+        que es como se almacenan en la base de datos.
+     */
     @TypeConverter
     public fun byteArrayToBitmap(imagenByteArray: ByteArray?):Bitmap?{
         try {
@@ -30,6 +43,9 @@ class ConversorImagen {
 
     }
 
+    /*
+        Este método dado un nombre de imagen, obtiene el id de la imagen almacenada en la carpeta drawable.
+     */
     public fun stringToID(nombre:String, context:Context):Int{
         return context.resources.getIdentifier(nombre, "drawable", "com.acl.easymeal")
     }
--- src/app/src/main/java/com/acl/easymeal/MainActivity.kt
+++ src/app/src/main/java/com/acl/easymeal/MainActivity.kt
@@ -8,7 +8,15 @@ import com.acl.easymeal.fragmentos.*
 import com.acl.easymeal.modelo.Receta
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+/*
+    Esta clase representa la actividad principal, su unica funcionalidad es definir la navegación entre
+    fragmentos, ya que el resto de comportamientos están definidos a lo largo de los diversos fragmentos.
+ */
 class MainActivity : AppCompatActivity() {
+    /*
+        Este método se llama cuando se está creando la actividad principal. Se llama al método
+        inicializaControlNavegacion que inicializa el menú inferior.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,40 +24,67 @@ class MainActivity : AppCompatActivity() {
         inicializaControlNavegacion()
     }
 
+    /*
+        Este método inicializa el menú inferior para navegar entre los distintos fragmentos.
+     */
     private fun inicializaControlNavegacion(){
         val menu_inferior = findViewById<BottomNavigationView>(R.id.menu_inferior)
         val controlador_navegacion = findNavController(R.id.fragment2)
         menu_inferior.setupWithNavController(controlador_navegacion)
     }
 
+    /*
+        Este método será utilizado para navegar del fragmento de Perfil al de Registro.
+     */
     public fun onRegisterSelected(){
         findNavController(R.id.fragment2).navigate(FragmentoperfilDirections.actionPerfilMenuItemToFragmentoRegister())
     }
 
+    /*
+        Este método será utilizado para navegar del fragmento de Registro al de Perfil.
+     */
     public fun fromRegisterToLogin(){
         findNavController(R.id.fragment2).navigate(FragmentoRegisterDirections.actionFragmentoRegisterToPerfilMenuItem())
     }
 
+    /*
+        Este método será utilizado para navegar del fragmento de Perfil al de Añadir receta.
+     */
     public fun fromPerfilToAnadirReceta(){
         findNavController(R.id.fragment2).navigate(FragmentoperfilDirections.actionPerfilMenuItemToFragmentoAnadirReceta())
     }
 
+    /*
+        Este método será utilizado para navegar del fragmento de Añadir receta al de Perfil.
+     */
     public fun fromAnadirRecetaToPerfil(){
         findNavController(R.id.fragment2).navigate(FragmentoAnadirRecetaDirections.actionFragmentoAnadirRecetaToPerfilMenuItem())
     }
 
+    /*
+        Este método será utilizado para navegar del fragmento de Categorías al de Recetas.
+     */
     public fun fromCategoriasToRecetas(categoria:String){
         findNavController(R.id.fragment2).navigate(FragmentoCategoriasDirections.actionCategoriasMenuItemToRecetasMenuItem(categoria))
     }
 
+    /*
+        Este método será utilizado para navegar del fragmento de Recetas al de Detalles de una receta.
+     */
     public fun fromRecetaToDetalles(idreceta: String){
         findNavController(R.id.fragment2).navigate(FragmentoListaRecetasDirections.actionRecetasMenuItemToFragmentoDescripcionReceta(idreceta))
     }
 
+    /*
+        Este método será utilizado para navegar del fragmento de Perfil al de Detalles de una receta.
+     */
     public fun fromPerfilToMostrarReceta(idreceta: String){
         findNavController(R.id.fragment2).navigate(FragmentoperfilDirections.actionPerfilMenuItemToFragmentoDescripcionReceta(idreceta))
     }
 
+    /*
+        Este método será utilizado para navegar del fragmento de Perfil al de Editar una receta.
+     */
     public fun fromPerfilToEditar(idreceta: String){
         findNavController(R.id.fragment2).navigate(FragmentoperfilDirections.actionPerfilMenuItemToFragmentoAnadirReceta("editar", idreceta))
     }
